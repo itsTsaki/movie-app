c05e045501d91d507c86546e4ad0a38f97f6f44e
Tsaki <itspranav.pk@gmail.com>
2025-04-04 16:16:10 +0530

    added loading effect

---
+++ app/loading.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center">
+      <Image src="beanEater.svg" width={500} height={500} alt="loading...."/>
+    </div>
+  )
+}
